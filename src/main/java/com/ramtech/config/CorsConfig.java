3a05acf308b1c5ffab727973831f536854be2d15
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2026-03-11 17:21:24 +0530

    Update CORS configuration to use allowedOriginPatterns

--- src/main/java/com/ramtech/config/CorsConfig.java
+++ src/main/java/com/ramtech/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
