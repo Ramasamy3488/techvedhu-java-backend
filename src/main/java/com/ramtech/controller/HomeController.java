b8db83e423860199dd024c89e6afdbe71074561c
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2026-03-12 12:42:34 +0530

    Add CORS support to HomeController

--- src/main/java/com/ramtech/controller/HomeController.java
+++ src/main/java/com/ramtech/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.ramtech.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {
+        "http://localhost:3000",
+        "https://ramasamyfullstack.github.io"
+})
 @RestController
 public class HomeController {
 
